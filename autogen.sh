49cad34f86b36b355afac409d2087c691f44de63
Dick Porter <dick@src.gnome.org>
1998-06-19 19:44:25 +0000

    Fri Jun 19 20:42:10 BST 1998  Dick Porter <dick@cymru.net>
    o Libtooled everything
    o Updated .cvsignore appropriately
    o Moved t.c into test/

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to compile GTK+."
+	echo "You must have autoconf installed to compile ORBit."
 	echo "Download the appropriate package for your distribution,"
 	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
@@ -13,7 +13,7 @@ DIE=0
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have libtool installed to compile GTK+."
+	echo "You must have libtool installed to compile ORBit."
 	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
@@ -21,7 +21,7 @@ DIE=0
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have automake installed to compile GTK+."
+	echo "You must have automake installed to compile ORBit."
 	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
@@ -32,7 +32,7 @@ if test "$DIE" -eq 1; then
 fi
 
 (test -f src/orb/orbit_types.h) || {
-	echo "You must run this script in the top-level GTK+ directory"
+	echo "You must run this script in the top-level ORBit directory"
 	exit 1
 }
 
@@ -44,7 +44,12 @@ fi
 for i in .
 do 
   echo processing $i
-  (cd $i; aclocal; autoheader; automake; autoheader; autoconf)
+  (cd $i; \
+    libtoolize --copy --force; \
+    aclocal; autoheader; \
+    automake --add-missing; \
+    autoheader; \
+    autoconf)
 done
 
 echo "Running ./configure --enable-maintainer-mode" "$@"
