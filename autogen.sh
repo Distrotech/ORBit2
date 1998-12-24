39fd98322eb5695c767816b85c35fb48ddb7454b
Elliot Lee <sopwith@src.gnome.org>
1998-12-24 01:05:57 +0000

    autogen.sh: Check for specific minimum versions of the various tools (automake, libtool, autoconf). Patch from Ronald Garcia <rgarcia4@darwin.helios.nd.edu>

--- autogen.sh
+++ autogen.sh
@@ -9,29 +9,36 @@ cd $srcdir
 
 DIE=0
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+# Check for autoconf
+(autoconf --version | grep -q "version 2.12") || {
 	echo
-	echo "You must have autoconf installed to compile ORBit."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "You must have at minimum autoconf version 2.12 installed"
+	echo "to compile ORBit. Download the appropriate package for"
+	echo "your distribution, or get the source tarball at"
+	echo "ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
+# Check for libtool
+(libtool --version | grep -q "1.2") || {
 	echo
-	echo "You must have libtool installed to compile ORBit."
-	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "You must have at minimum libtool version 1.2 installed"
+	echo "to compile ORBit. Download the appropriate package for"
+	echo "your distribution, or get the source tarball at"
+	echo "ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
 	DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+# Check for automake
+(automake --version | grep -q "1.3") ||{
 	echo
-	echo "You must have automake installed to compile ORBit."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "You must have at minimum libtool version 1.3 installed"
+	echo "to compile ORBit. Download the appropriate package for"
+	echo "your distribution, or get the source tarball at"
+	echo "ftp://ftp.cygnus.com/pub/home/tromey/automake-1.3.tar.gz"
 	DIE=1
 }
+
 
 if test "$DIE" -eq 1; then
 	exit 1
