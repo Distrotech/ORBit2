0098f10463bc57038d6e6bcb09dd28bcfa2da98a
Elliot Lee <sopwith@src.gnome.org>
1999-05-06 22:05:06 +0000

    Basic skel.
    Basic skel.

---
+++ src/idl-compiler/orbit-idl-c-backend.h
@@ -0,0 +1,10 @@
+#ifndef ORBIT_IDL_C_BACKEND_H
+#define ORBIT_IDL_C_BACKEND_H
+
+#include "orbit-idl2.h"
+
+#include <unistd.h>
+
+void orbit_idl_output_c(OIDL_Output_Tree *tree, OIDL_Run_Info *rinfo);
+
+#endif
