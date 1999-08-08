b343b457817690146f56389eb3d1f350521109dd
Martin Baulig <martin@src.gnome.org>
1999-08-08 22:43:53 +0000

    Fixed the bug I reported to the orbit-list that afternoon; we need to use
    #define NEEDS_INDIRECT(node) (((node)->flags & MN_LOOPED) || ((node)->flags & MN_NEED_TMPVAR))
    
    here (it had a `&&' before).

--- src/idl-compiler/orbit-idl-c-marshal.c
+++ src/idl-compiler/orbit-idl-c-marshal.c
@@ -2,7 +2,7 @@
 
 #include "orbit-idl-c-backend.h"
 
-#define NEEDS_INDIRECT(node) (((node)->flags & MN_LOOPED) && ((node)->flags & MN_NEED_TMPVAR))
+#define NEEDS_INDIRECT(node) (((node)->flags & MN_LOOPED) || ((node)->flags & MN_NEED_TMPVAR))
 
 static void c_marshal_generate(OIDL_Marshal_Node *node, OIDL_C_Marshal_Info *cmi);
 static void c_marshal_datum(OIDL_Marshal_Node *node, OIDL_C_Marshal_Info *cmi);
