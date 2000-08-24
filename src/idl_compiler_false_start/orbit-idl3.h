b6fae7126d125f2c027e1013482648ee8df5cf45
Elliot Lee <sopwith@src.gnome.org>
2000-08-24 00:20:35 +0000

    *** empty log message ***

---
+++ src/idl_compiler_false_start/orbit-idl3.h
@@ -0,0 +1,31 @@
+#ifndef ORBIT_IDL3_H
+#define ORBIT_IDL3_H 1
+
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <libIDL/IDL.h>
+#include <orbit/util/basic_types.h>
+
+#define _ORBIT_H_ /* kludge - we only want ORBIT_SERIAL */
+#include <orbit/orbit.h>
+
+typedef struct {
+  char *cpp_args;
+  int debug_level;
+
+  enum { OIDL_OUTPUT_STUBS=1<<0,
+	 OIDL_OUTPUT_SKELS=1<<1,
+	 OIDL_OUTPUT_COMMON=1<<2,
+	 OIDL_OUTPUT_HEADERS=1<<3,
+	 OIDL_OUTPUT_SKELIMPL=1<<4 } enabled_passes;
+
+  char *output_formatter;
+
+  char *output_language;
+  char *input_filename;
+} OIDL_Run_Info;
+
+int orbit_idl_run(const char *filename, OIDL_Run_Info *rinfo);
+
+#endif
