01abb52f903f87a18f48e734b315248e84445cf8
Elliot Lee <sopwith@src.gnome.org>
2000-09-13 23:58:13 +0000

    Not so much today, just a little reorganization, switch to glib-2.0, and
    Not so much today, just a little reorganization, switch to glib-2.0, and ponderance of IIOP
    routines.

---
+++ linc2/include/linc/linc-types.h
@@ -0,0 +1,13 @@
+#ifndef GIOP_TYPES_H
+#define GIOP_TYPES_H 1
+
+#include <glib.h>
+
+typedef enum _GIOPReplyStatusType GIOPReplyStatusType;
+typedef enum _GIOPLocateStatusType GIOPLocateStatusType;
+typedef struct _GIOPMsgRequest GIOPMsgRequest;
+typedef struct _GIOPMsgReply GIOPMsgReply;
+typedef struct _GIOPMsgCancelRequest GIOPMsgCancelRequest;
+typedef struct _GIOPMsgLocateRequest GIOPMsgLocateRequest;
+typedef struct _GIOPMsgLocateReply GIOPMsgLocateReply;
+typedef struct _GIOPMsg GIOPMsg;
