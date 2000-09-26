f3afbdf5128a8a5bac1eae1710a6209d2007f660
Elliot Lee <sopwith@src.gnome.org>
2000-09-26 20:09:19 +0000

    Commit changes so I can rename this file.

--- src/orb/orb-core/corba-orb.c
+++ src/orb/orb-core/corba-orb.c
@@ -1,13 +1,34 @@
 #include <orbit/orb-core/orb-core.h>
 #include <linc/linc.h>
 
+static void
+CORBA_ORB_destroy(ORBit_RootObject robj)
+{
+  CORBA_ORB orb = (CORBA_ORB)robj;
+
+  g_list_foreach(orb->servers, (GFunc)g_object_unref, NULL);
+  g_ptr_array_free(orb->poas, TRUE);
+
+  g_free(obj);
+}
+
 CORBA_ORB
 CORBA_ORB_init(int *argc, char **argv, CORBA_ORBid orb_identifier, CORBA_Environment *ev)
 {
-  CORBA_ORB retval = NULL;
+  static CORBA_ORB retval = NULL;
   LINCProtocolInfo *info;
+  static ORBit_RootObject_Interface orb_if = {
+    ORBIT_ROT_ORB,
+    CORBA_ORB_destroy
+  };
+
+  if(retval)
+    return (CORBA_ORB)CORBA_Object_duplicate((CORBA_Object)orb);
 
   retval = g_new0(struct _CORBA_ORB, 1);
+  orb = (CORBA_ORB)retval;
+
+  ORBit_RootObject_init(retval, &orb_if);
 
   for(info = linc_protocol_all(); info->name; info++)
     {
@@ -26,12 +47,9 @@ CORBA_ORB_init(int *argc, char **argv, CORBA_ORBid orb_identifier, CORBA_Environ
 	}
     }
 
-  return retval;
-}
+  retval->poas = g_ptr_array_new();
 
-CORBA_Current
-CORBA_ORB_get_current(CORBA_ORB orb, CORBA_Environment *ev)
-{
+  return retval;
 }
 
 /* ORBit extension */
