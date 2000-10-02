f9b3bcb1e4af000d43d0b9726285a6c70b40f670
Elliot Lee <sopwith@src.gnome.org>
2000-10-02 23:53:35 +0000

    Missed files.
    Missed files.

---
+++ src/orb/orb-core/corba-policy.c
@@ -0,0 +1,30 @@
+CORBA_PolicyType
+CORBA_Policy__get_policy_type(CORBA_Policy _obj,
+			      CORBA_Environment * ev)
+{
+}
+
+CORBA_Policy
+CORBA_Policy_copy(CORBA_Policy _obj, CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Policy_destroy(CORBA_Policy _obj, CORBA_Environment * ev)
+{
+}
+
+CORBA_Policy
+CORBA_DomainManager_get_domain_policy(CORBA_DomainManager _obj,
+				      const CORBA_PolicyType policy_type,
+				      CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_ConstructionPolicy_make_domain_manager(CORBA_ConstructionPolicy _obj,
+					     const CORBA_InterfaceDef object_type,
+					     const CORBA_boolean constr_policy,
+					     CORBA_Environment * ev)
+{
+}
