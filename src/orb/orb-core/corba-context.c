6ca2769d873a808714c1044375f8cb5620274bfd
Elliot Lee <sopwith@src.gnome.org>
2000-10-02 23:53:35 +0000

    Missed files.

---
+++ src/orb/orb-core/corba-context.c
@@ -0,0 +1,45 @@
+void
+CORBA_Context_set_one_value(CORBA_Context _obj,
+			    const CORBA_Identifier prop_name,
+				    const CORBA_char * value,
+				    CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Context_set_values(CORBA_Context _obj,
+			      const CORBA_NVList values,
+			      CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Context_get_values(CORBA_Context _obj,
+			      const CORBA_Identifier start_scope,
+			      const CORBA_Flags op_flags,
+			      const CORBA_Identifier prop_name,
+			      CORBA_NVList * values,
+			      CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Context_delete_values(CORBA_Context _obj,
+				 const CORBA_Identifier prop_name,
+				 CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Context_create_child(CORBA_Context _obj,
+				const CORBA_Identifier ctx_name,
+				CORBA_Context * child_ctx,
+				CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Context_delete(CORBA_Context _obj, const CORBA_Flags del_flags,
+			  CORBA_Environment * ev)
+{
+}
---
+++ src/orb/orb-core/corba-request.c
@@ -0,0 +1,39 @@
+void
+CORBA_Request_add_arg(CORBA_Request _obj, const CORBA_Identifier name,
+		      const CORBA_TypeCode arg_type,
+		      const CORBA_OpaqueValue value,
+		      const CORBA_long len,
+		      const CORBA_Flags arg_flags,
+		      CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Request_invoke(CORBA_Request _obj,
+		     const CORBA_Flags invoke_flags,
+		     CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Request_delete(CORBA_Request _obj, CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Request_send(CORBA_Request _obj, const CORBA_Flags invoke_flags,
+		   CORBA_Environment * ev)
+{
+}
+
+void
+CORBA_Request_get_response(CORBA_Request _obj,
+			   CORBA_Environment * ev)
+{
+}
+
+CORBA_boolean
+CORBA_Request_poll_response(CORBA_Request _obj,
+			    CORBA_Environment * ev)
+{
+}
