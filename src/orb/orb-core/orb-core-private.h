0b97a196560fcb5e5c97425bfad647ce65b1741d
Elliot Lee <sopwith@src.gnome.org>
2000-10-17 01:15:18 +0000

    Missed this - header for private orb-core stuff.
    Missed this - header for private orb-core stuff.

---
+++ src/orb/orb-core/orb-core-private.h
@@ -0,0 +1,11 @@
+#ifndef ORB_CORE_PRIVATE_H
+#define ORB_CORE_PRIVATE_H 1
+
+#include <orbit/orbit.h>
+
+CORBA_TypeCode ORBit_get_union_tag(CORBA_TypeCode union_tc, gconstpointer *val,
+				   gboolean update);
+size_t ORBit_gather_alloc_info(CORBA_TypeCode tc);
+gint ORBit_find_alignment(CORBA_TypeCode tc);
+
+#endif
