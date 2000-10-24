dc16984a661d9bb8f08a5b7a6d62bc6eeaff866b
Elliot Lee <sopwith@src.gnome.org>
2000-10-24 01:32:40 +0000

    Add new files.
    Add new files.

---
+++ src/orb/poa/orbit-poa-export.h
@@ -0,0 +1,7 @@
+#ifndef ORBIT_POA_EXPORT_H
+#define ORBIT_POA_EXPORT_H
+
+/* Stuff that is internal to ORBit but not internal to the POA */
+void ORBit_handle_request(CORBA_ORB orb, GIOPRecvBuffer *buf);
+
+#endif
