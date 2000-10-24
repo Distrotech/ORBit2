4595023d510fa9fbd49b0546ecf986a0c5daac89
Elliot Lee <sopwith@src.gnome.org>
2000-10-24 01:32:40 +0000

    Add new files.

---
+++ src/orb/orb-core/orb-core-export.h
@@ -0,0 +1,8 @@
+#ifndef ORB_CORE_EXPORT_H
+#define ORB_CORE_EXPORT_H 1
+
+void ORBit_impl_CORBA_Object_is_a(gpointer servant,
+				  GIOPRecvBuffer * _ORBIT_recv_buffer,
+				  CORBA_Environment *ev,
+				  gpointer dummy_impl);
+#endif
