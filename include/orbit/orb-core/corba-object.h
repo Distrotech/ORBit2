ef9ba7d92ee11623bc852789887084e290439952
Michael Meeks <michael@ximian.com>
2001-07-17 22:18:57 +0000

    rename to (ORBit_object_get_connection): this & re-order slightly.
    2001-07-18  Michael Meeks  <michael@ximian.com>
    
    	* src/orb/orb-core/corba-object.c
    	(_ORBit_object_get_connection): rename to
    	(ORBit_object_get_connection): this & re-order slightly.
    	(CORBA_Object_non_existent): local objects always exist,
    	even if we havn't bothered remoting them yet => no
    	connection.
    
    	* include/orbit/orb-core/corba-object.h: kill macro
    	(ORBit_object_get_connection): somewhat confusing.
    
    	* test/everything/client.c (testIInterface): add
    	CORBA_Object_non_existent and CORBA_Object_is_nil
    	regression tests.

--- include/orbit/orb-core/corba-object.h
+++ include/orbit/orb-core/corba-object.h
@@ -1,10 +1,7 @@
 #ifndef CORBA_OBJECT_H
 #define CORBA_OBJECT_H 1
 
-#define ORBit_object_get_connection(obj) \
-     (((obj)->connection && (LINC_CONNECTION((obj)->connection)->status == LINC_CONNECTED))?((obj)->connection):_ORBit_object_get_connection(obj))
-
-GIOPConnection *_ORBit_object_get_connection(CORBA_Object obj);
+GIOPConnection *ORBit_object_get_connection(CORBA_Object obj);
 
 void ORBit_marshal_object(GIOPSendBuffer *buf, CORBA_Object obj);
 gboolean ORBit_demarshal_object(CORBA_Object *obj, GIOPRecvBuffer *buf,
