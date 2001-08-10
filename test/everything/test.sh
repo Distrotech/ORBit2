4adc60a55a41fac8e23700d28f40a10555ab63ba
Michael Meeks <michael@ximian.com>
2001-08-10 20:54:03 +0000

    trash /tmp/orbit-$USER before we start ...
    2001-08-10  Michael Meeks  <michael@ximian.com>
    
    	* test/everything/test.sh: trash /tmp/orbit-$USER before
    	we start ...

--- test/everything/test.sh
+++ test/everything/test.sh
@@ -3,6 +3,9 @@
 # This is a generic script for firing up a server, waiting for it to write
 # its stringified IOR to a file, then firing up a server
 
+# We had issues creating this once, and it gives us a 'clean' slate.
+rm -Rf "/tmp/orbit-$USER"
+
 ./server&
 
 until test -s iorfile; do sleep 1; done
