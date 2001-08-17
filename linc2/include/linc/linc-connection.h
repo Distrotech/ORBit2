ac6c5a0469180443ad4133906d83c5ee3c27b938
Michael Meeks <michael@src.gnome.org>
2001-08-17 02:57:56 +0000

    2001-08-16  Michael Meeks  <michael@ximian.com>
    	* configure.in: bump version to 0.1.2
    
    	* src/linc-connection.c (linc_connection_get_type): don't
    	use the base_init - but the instance_init to set fd == -1
    	so we don't close (0), doh.
    	(linc_connection_connected): update to call the
    	handle_input virtual method on input.

--- linc2/include/linc/linc-connection.h
+++ linc2/include/linc/linc-connection.h
@@ -15,7 +15,7 @@
 #define LINC_TYPE_IS_CONNECTION(type)   (G_TYPE_FUNDAMENTAL (type) == LINC_TYPE_CONNECTION)
 #define LINC_CONNECTION(object)	        (LINC_IS_CONNECTION (object) ? ((LINCConnection*) (object)) : \
 				         G_TYPE_CHECK_INSTANCE_CAST ((object), LINC_TYPE_CONNECTION, LINCConnection))
-#define LINC_CONNECTION_CLASS(class)    (LINC_IS_CONNECTION_CLASS (class) ? ((GObjectClass*) (class)) : \
+#define LINC_CONNECTION_CLASS(class)    (LINC_IS_CONNECTION_CLASS (class) ? ((LINCConnectionClass*) (class)) : \
 				         G_TYPE_CHECK_CLASS_CAST ((class), LINC_TYPE_CONNECTION, LINCConnectionClass))
 #define LINC_IS_CONNECTION(object)      (((LINCConnection*) (object)) != NULL && \
 				         LINC_IS_CONNECTION_CLASS (((GTypeInstance*) (object))->g_class))
@@ -49,10 +49,12 @@ typedef struct {
 	GObjectClass parent_class;
 
 	/* subclasses should call parent impl first */
-	void (* state_changed)(LINCConnection *cnx, LINCConnectionStatus status);
+	void     (* state_changed) (LINCConnection *cnx, LINCConnectionStatus status);
+
+	gboolean (* handle_input)  (LINCConnection *cnx);
 
 	/* signals */
-	void (* broken) (LINCConnection *cnx);
+	void     (* broken) (LINCConnection *cnx);
 } LINCConnectionClass;
 
 GType    linc_connection_get_type (void) G_GNUC_CONST;
