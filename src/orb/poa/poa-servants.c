c7bb158c4e3503a1fb4744951f7c676de3cdf789
Mark McLoughlin <mark@skynet.ie>
2001-08-20 15:10:21 +0000

    more cleanup.
    2001-08-20  Mark McLoughlin <mark@skynet.ie>
    
            * src/orb/poa/poa-servants.c,
              src/orb/poa/poa-manager.c,
              src/orb/poa/poa.c: more cleanup.

--- src/orb/poa/poa-servants.c
+++ src/orb/poa/poa-servants.c
@@ -1,23 +1,22 @@
 #include <orbit/orbit.h>
-#include "orbit-poa.h"
 
 PortableServer_POA
-PortableServer_ServantBase__default_POA(PortableServer_Servant servant,
-					CORBA_Environment *ev)
+PortableServer_ServantBase__default_POA (PortableServer_Servant  servant,
+					 CORBA_Environment      *ev)
 {
-  g_return_val_if_fail(servant, NULL);
+	g_return_val_if_fail (servant, NULL);
 
-  return ORBIT_SERVANT_TO_FIRST_POAOBJECT(servant)->poa;
+	return ORBIT_SERVANT_TO_FIRST_POAOBJECT (servant)->poa;
 }
 
 void
-PortableServer_ServantBase__init(PortableServer_Servant p_servant,
-				 CORBA_Environment *ev)
+PortableServer_ServantBase__init (PortableServer_Servant  p_servant,
+				  CORBA_Environment      *ev)
 {
 }
 
 void
-PortableServer_ServantBase__fini(PortableServer_Servant p_servant,
-				 CORBA_Environment *ev)
+PortableServer_ServantBase__fini (PortableServer_Servant  p_servant,
+				  CORBA_Environment      *ev)
 {
 }
