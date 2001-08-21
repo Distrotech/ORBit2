01ddf791c31eae3240c9bd38fb75f14be54594a6
Mark McLoughlin <mark@skynet.ie>
2001-08-21 08:45:41 +0000

    src/orb/orb-core/poa.c (ORBit_POAObject_object_to_objkey) removed objid.
    2001-08-20  Mark McLoughlin <mark@skynet.ie>
    
            * src/orb/orb-core/poa.c
            (ORBit_POAObject_object_to_objkey) removed
            objid.
            (ORBit_ORB_find_POA_for_okey) killed.

--- include/orbit/poa/poa-basics.h
+++ include/orbit/poa/poa-basics.h
@@ -6,7 +6,7 @@ typedef struct ORBit_OAObject_type  *ORBit_OAObject;
 
 #if !defined(_PortableServer_Servant_defined)
 #define _PortableServer_Servant_defined 1
-   typedef gpointer PortableServer_Servant;
+	typedef gpointer PortableServer_Servant;
 #else
 #error "Include mixup: poa-defs.h included before poa-basics.h"
 #endif
@@ -14,41 +14,46 @@ typedef struct ORBit_OAObject_type  *ORBit_OAObject;
 #if !defined(ORBIT_DECL_PortableServer_POA) && !defined(_PortableServer_POA_defined)
 #define ORBIT_DECL_PortableServer_POA 1
 #define _PortableServer_POA_defined 1
-   typedef struct PortableServer_POA_type *PortableServer_POA;
+	typedef struct PortableServer_POA_type *PortableServer_POA;
 #endif
 
 typedef struct {
-  void *_private;
-  void (*finalize)(PortableServer_Servant servant, CORBA_Environment *ev);
-  PortableServer_POA (*default_POA)(PortableServer_Servant servant,
-                                    CORBA_Environment *ev);
+	void                *_private;
+	void               (*finalize)    (PortableServer_Servant  servant,
+					   CORBA_Environment      *ev);
+	PortableServer_POA (*default_POA) (PortableServer_Servant  servant,
+					   CORBA_Environment      *ev);
 } PortableServer_ServantBase__epv;
 
 typedef PortableServer_ServantBase__epv *PortableServer_ServantBase__vepv;
 
 typedef struct {
-  void *_private;
-  PortableServer_ServantBase__vepv *vepv;
+	void                             *_private;
+	PortableServer_ServantBase__vepv *vepv;
 } PortableServer_ServantBase;
 
 typedef gshort ORBit_VepvIdx;
 
-typedef void (*ORBitSkeleton)(PortableServer_ServantBase *_ORBIT_servant,
-                              gpointer _ORBIT_recv_buffer,
-                              CORBA_Environment *ev,
-                              gpointer implementation);
+typedef void               (*ORBitSkeleton)      (PortableServer_ServantBase *servant,
+						  gpointer                    recv_buffer,
+						  CORBA_Environment          *ev,
+						  gpointer                    implementation);
 
-typedef void (*ORBitSmallSkeleton) (PortableServer_ServantBase *_ORBIT_servant,
-                                    gpointer ret, gpointer *args,
-                                    gpointer ctx, CORBA_Environment *ev,
-                                    gpointer implementation);
+typedef void               (*ORBitSmallSkeleton) (PortableServer_ServantBase *servant,
+						  gpointer                    ret,
+						  gpointer                   *args,
+						  gpointer                    ctx,
+						  CORBA_Environment          *ev,
+						  gpointer                    implementation);
 
-typedef ORBitSkeleton (*ORBit_impl_finder)(PortableServer_ServantBase *servant,
-                                           gpointer _ORBIT_recv_buffer,
-                                           gpointer *implementation);
-typedef ORBitSmallSkeleton (*ORBit_small_impl_finder)(PortableServer_ServantBase *servant,
-                                                      const char                 *method,
-                                                      gpointer                   *m_data,
-                                                      gpointer                   *implementation);
+typedef ORBitSkeleton      (*ORBit_impl_finder)  (PortableServer_ServantBase *servant,
+						  gpointer                    recv_buffer,
+						  gpointer                   *implementation);
+
+typedef ORBitSmallSkeleton (*ORBit_small_impl_finder)
+						 (PortableServer_ServantBase *servant,
+						  const char                 *method,
+						  gpointer                   *m_data,
+						  gpointer                   *implementation);
 
 #endif /* _POA_BASICS_H_ 1 */
