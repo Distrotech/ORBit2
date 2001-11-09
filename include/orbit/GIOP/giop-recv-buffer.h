96b61f4072ccd6d01ee01075bb881a756b1ca312
Michael Meeks <michael@src.gnome.org>
2001-11-09 04:41:17 +0000

    2001-11-09  Michael Meeks  <michael@ximian.com>
    	* include/orbit/GIOP/giop-recv-buffer.h: kill
    	GIOPMessageInfo, not neccessary.
    	remove giop_recv_buffer_data_read prototype.
    
    	* src/orb/GIOP/giop-recv-buffer.c
    	(giop_recv_list_push): a crack smoking & inefficient
    	loop search; re-order compare - most unlikely first,
    	get request_id only once. re-order locking to be yet
    	more transparent. Remove the huge, paranoid and
    	incorrect ( except for Elliot's huge stubs/skels )
    	message.
    	(giop_recv_buffer_demarshal): make robust against
    	duff giop versions.
    	(giop_recv_buffer_use_buf): simplify, remove bogus
    	is_auth parameter.
    	(giop_connection_handle_input): upd. to not pass isauth.
    	(giop_recv_list_push): add processed concept outline.

--- include/orbit/GIOP/giop-recv-buffer.h
+++ include/orbit/GIOP/giop-recv-buffer.h
@@ -13,12 +13,6 @@ typedef enum {
   GIOP_MSG_READY
 } GIOPMessageBufferState;
 
-typedef enum {
-  GIOP_MSG_UNDERWAY,
-  GIOP_MSG_COMPLETE,
-  GIOP_MSG_INVALID
-} GIOPMessageInfo;
-
 typedef struct _GIOPMessageQueueEntry GIOPMessageQueueEntry;
 
 typedef void (*GIOPAsyncCallback) (GIOPMessageQueueEntry *ent);
@@ -59,7 +53,7 @@ struct _GIOPRecvBuffer {
 };
 
 #define         giop_msg_conversion_needed(msg)     giop_endian_conversion_needed(GIOP_MSG(msg)->header.flags)
-GIOPRecvBuffer *giop_recv_buffer_use_buf           (gboolean               is_auth);
+GIOPRecvBuffer *giop_recv_buffer_use_buf           (void);
 GIOPRecvBuffer *giop_recv_buffer_use_encaps_buf    (GIOPRecvBuffer        *buf);
 GIOPRecvBuffer *giop_recv_buffer_use_encaps        (guchar                *mem,
 						    gulong                 len);
@@ -76,9 +70,6 @@ GIOPRecvBuffer *giop_recv_buffer_get               (GIOPMessageQueueEntry *ent,
 
 GIOPRecvBuffer *giop_recv_buffer_use               (void);
 void            giop_recv_buffer_unuse             (GIOPRecvBuffer        *buf);
-GIOPMessageInfo giop_recv_buffer_data_read         (GIOPRecvBuffer        *buf,
-						    int n, gboolean is_auth,
-						    GIOPConnection        *cnx);
 
 #define giop_recv_buffer_reply_status(buf) (                                            \
 	(buf)->msg.header.version [1] ==  2 ? (buf)->msg.u.reply_1_2.reply_status :     \
