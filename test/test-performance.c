92fc38dd83ad74d42a2b19a428deb0907721e733
Mark McLoughlin <mark@skynet.ie>
2002-01-09 11:40:52 +0000

    add object activation/de-activation performance test.
    2002-01-09  Mark McLoughlin  <mark@skynet.ie>
    
            * test/test-performance.c: (create_mult_id_poa), (test_activation),
            (main): add object activation/de-activation performance test.
    
            * test/Makefile.am: upd.

--- test/test-performance.c
+++ test/test-performance.c
@@ -4,9 +4,12 @@
 
 #include <orbit/orbit.h>
 
-static GTimer *timer;
-static double  bogomark = 0.0;
-static double  elapsed_time;
+#include "test1.h"
+
+static CORBA_ORB  orb;
+static GTimer    *timer;
+static double     bogomark = 0.0;
+static double     elapsed_time;
 
 static void
 test_copy (void)
@@ -54,11 +57,111 @@ test_copy (void)
 	fprintf (stderr, " strdup : %g(ms)\n", elapsed_time);
 }
 
+static PortableServer_POA
+create_mult_id_poa (CORBA_Environment *ev)
+{
+	PortableServer_POA  rootpoa;
+	PortableServer_POA  retval;
+	CORBA_PolicyList   *policies;
+
+	rootpoa = (PortableServer_POA)
+		CORBA_ORB_resolve_initial_references (orb, "RootPOA", ev);
+	g_assert (ev->_major == CORBA_NO_EXCEPTION);
+
+	policies           = CORBA_PolicyList__alloc ();
+	policies->_maximum = 1;
+	policies->_length  = 1;
+	policies->_buffer  = CORBA_PolicyList_allocbuf (1);
+	CORBA_sequence_set_release (policies, CORBA_TRUE);
+
+	policies->_buffer[0] = (CORBA_Policy)
+					PortableServer_POA_create_id_uniqueness_policy (
+							rootpoa,
+							PortableServer_MULTIPLE_ID,
+							ev);
+	g_assert (ev->_major == CORBA_NO_EXCEPTION);
+
+	retval = PortableServer_POA_create_POA (rootpoa, "Multiple Id POA",
+					        NULL, policies, ev);
+	g_assert (ev->_major == CORBA_NO_EXCEPTION);
+
+	CORBA_Policy_destroy (policies->_buffer[0], ev);
+	CORBA_free (policies);
+
+	g_assert (ev->_major == CORBA_NO_EXCEPTION);
+
+	CORBA_Object_release ((CORBA_Object) rootpoa, ev);
+
+	g_assert (ev->_major == CORBA_NO_EXCEPTION);
+
+	return retval;
+}
+
+PortableServer_ServantBase__epv base_epv = { NULL, NULL, NULL };
+POA_Test__epv                   test_epv = { NULL, NULL };
+POA_Test__vepv                  test_vepv = { &base_epv, &test_epv };
+POA_Test                        test_servant = { NULL, &test_vepv };
+
+static void
+test_activation (void)
+{
+	CORBA_Environment   env;
+	PortableServer_POA  poa;
+	GSList             *objids = NULL, *l;
+	int                 i;
+
+	fprintf (stderr, "Testing object activation...\n");
+
+	CORBA_exception_init (&env);
+
+	POA_Test__init (&test_servant, &env);
+
+	poa = create_mult_id_poa (&env);
+
+	g_assert (env._major == CORBA_NO_EXCEPTION);
+
+	g_timer_reset (timer);
+
+	for (i = 0; i < 1000; i++) {
+		PortableServer_ObjectId *objid;
+
+		objid = PortableServer_POA_activate_object (poa, &test_servant, &env);
+		g_assert (env._major == CORBA_NO_EXCEPTION);
+
+		objids = g_slist_append (objids, objid);
+	}
+
+	elapsed_time = g_timer_elapsed (timer, NULL);
+	bogomark += elapsed_time;
+	fprintf (stderr, " activation : %g(ms)\n", elapsed_time);
+
+	g_timer_reset (timer);
+
+	for (l = objids; l; l = l->next) {
+		PortableServer_POA_deactivate_object (poa, l->data, &env);
+		g_assert (env._major == CORBA_NO_EXCEPTION);
+	}
+
+	elapsed_time = g_timer_elapsed (timer, NULL);
+	bogomark += elapsed_time;
+	fprintf (stderr, " de-activation : %g(ms)\n", elapsed_time);
+	
+	for (l = objids; l; l = l->next)
+		CORBA_free (l->data);
+	g_slist_free (objids);
+
+	POA_Test__fini (&test_servant, &env);
+
+	CORBA_Object_release ((CORBA_Object) poa, &env);
+	g_assert (env._major == CORBA_NO_EXCEPTION);
+
+	CORBA_exception_free (&env);
+}
+
 int
 main (int argc, char *argv[])
 {
 	CORBA_Environment ev;
-	CORBA_ORB    orb;
 
 	free (malloc (8));
 
@@ -75,6 +178,8 @@ main (int argc, char *argv[])
 	bogomark += elapsed_time;
 
 	test_copy ();
+
+	test_activation ();
 
 	g_timer_reset (timer);
 	CORBA_ORB_destroy (orb, &ev);
