e9a0b832d22fa7f9732141bdb930649a6a9b6ab5
Mark McLoughlin <mmclouglin@src.gnome.org>
2002-01-16 12:56:44 +0000

    2002-01-16  Mark McLoughlin  <mark@skynet.ie>
            * include/orbit/orb-core/corba-orb-type.h: kill genrand
            ORB member.
    
            * include/orbit/util/orbit-genrand.h: rename to genuid, we're
            not allways taking about randomness.
    
            * src/orb/orb-core/corba-orb.c:
            (ORBit_genuid_type): impl to figure what genuid type we'll use.
            (CORBA_ORB_init): upd.
            (ORBit_proto_use): fix silly.
            Also add a --ORBSimpleUIDs option.
    
            * src/orb/poa/orbit-adaptor.c: (ORBit_adaptor_setup): upd.
    
            * src/orb/poa/poa.c: (ORBit_POA_new_system_objid): upd.
    
            * src/orb/util/genrand.c:
            (ORBit_genuid_init), (ORBit_genuid_fini): impl.
            (genuid_glib_pseudo): impl id generator using glib's prng.
            (genrand_unix): kill.
            (genuid_simple): impl non-random id generator.
            (ORBit_genuid_buffer): upd.
    
            * test/test-performance.c: (main): pass argc and argv to
            ORB_init.
    
            * docs/FAQ, docs/internals/poa-notes.txt: upd.

--- src/orb/util/genrand.c
+++ src/orb/util/genrand.c
@@ -1,38 +1,66 @@
 #include <config.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include <signal.h>
+#include <unistd.h>
 #include <sys/time.h>
-#include <limits.h>
-#include <stdio.h>
-#include <string.h>
+
 #include <glib.h>
 #include <linc/linc.h>
-#include <orbit/util/orbit-util.h>
+#include <orbit/util/orbit-genrand.h>
+
 #include "orbit-purify.h"
 
+static ORBitGenUidType  genuid_type = ORBIT_GENUID_STRONG;
+static int              dev_urandom_fd = -1;
+static GRand           *glib_prng = NULL;
+
 void
-ORBit_genrand_init (ORBit_genrand *gr)
+ORBit_genuid_init (ORBitGenUidType type)
 {
-	gr->fd = open ("/dev/urandom", O_RDONLY);
+	genuid_type = type;
+
+	switch (genuid_type) {
+	case ORBIT_GENUID_STRONG:
+		dev_urandom_fd = open ("/dev/urandom", O_RDONLY);
+
+		glib_prng = g_rand_new ();
+		break;
+	case ORBIT_GENUID_SIMPLE:
+		break;
+	default:
+		g_assert_not_reached ();
+		break;
+	}
 }
 
 void
-ORBit_genrand_fini (ORBit_genrand *gr)
+ORBit_genuid_fini (void)
 {
-	if (gr->fd >= 0) {
-		close (gr->fd); 
-		gr->fd = -1;
+	switch (genuid_type) {
+	case ORBIT_GENUID_STRONG:
+		if (dev_urandom_fd >= 0)
+			close (dev_urandom_fd); 
+		dev_urandom_fd = 0;
+
+		g_rand_free (glib_prng);
+		glib_prng = NULL;
+		break;
+	case ORBIT_GENUID_SIMPLE:
+		break;
+	default:
+		g_assert_not_reached ();
+		break;
 	}
 }
 
 static gboolean
-genrand_dev (ORBit_genrand *gr, guchar *buffer, int buf_len)
+genuid_rand_device (guchar *buffer, int length)
 {
-	if (read (gr->fd, buffer, buf_len) < buf_len) {
-		close (gr->fd);
-		gr->fd = -1;
+	if (read (dev_urandom_fd, buffer, length) < length) {
+		close (dev_urandom_fd);
+		dev_urandom_fd = -1;
 
 		return FALSE;
 	}
@@ -44,106 +72,69 @@ genrand_dev (ORBit_genrand *gr, guchar *buffer, int buf_len)
 #include <openssl/rand.h>
 
 static gboolean
-genrand_openssl (guchar *buffer, int buf_len)
+genrand_openssl (guchar *buffer, int length)
 {
 	static RAND_METHOD *rm = NULL;
 
 	if (!rm)
 		rm = RAND_get_rand_method ();
 
-	RAND_bytes (buffer, buf_len);
+	RAND_bytes (buffer, length);
 
 	return TRUE;
 }
 #endif
 
-static inline guchar
-hashlong (long val)
+static void
+genuid_glib_pseudo (guchar *buffer, int length)
 {
-	guchar retval = 0; /* Quiet gcc */
-	guchar *ptr;
-	int i;
+	GTimeVal time;
+	int      i;
 
-	for (ptr = (guchar *) &val, i = 0; i < sizeof (val); i++)
-		retval ^= ptr [i];
+	g_get_current_time (&time);
 
-	return retval;
+	g_rand_set_seed (glib_prng, time.tv_sec ^ time.tv_usec);
+
+	for (i = 0; i < length; i++)
+		buffer [i] = g_rand_int_range (glib_prng, 0, 255);
 }
-
-static volatile int received_alarm = 0;
 
 static void
-handle_alarm (int signum)
+genuid_simple (guchar *buffer, int length)
 {
-	received_alarm = 1;
-}
+	static glong s = 0x6b842128;
+	glong        i, t;
+	GTimeVal     time;
 
-/* FIXME: this method really sucks */
-static gboolean
-genrand_unix (guchar *buffer, int buf_len)
-{
-	struct sigaction sa, oldsa;
-	struct itimerval it, oldit;
-	int i;
-	long min, max;
-	long *counts;
-	double diff;
-	long *uninit;
+	g_get_current_time (&time);
 
-	counts = alloca (buf_len * sizeof (long));
+	t = time.tv_sec ^ time.tv_usec;
 
-	memset (&sa, 0, sizeof (sa));
-	sa.sa_handler = handle_alarm;
-	sigaction (SIGALRM, &sa, &oldsa);
-	memset (&it, 0, sizeof(it));
-	it.it_value.tv_usec = 1;
-	getitimer (ITIMER_REAL, &oldit);
+	for (i = 0; i < length; i++)
+		buffer [i] = (guchar) (s ^ (t << i));
 
-	for (i = 0, min = LONG_MAX, max = 0; i < buf_len; i++) {
-		received_alarm = 0;
-		setitimer (ITIMER_REAL, &it, NULL);
-		for (counts [i] = 0; !received_alarm; counts [i]++)
-			;
-
-		max = MAX (counts[i], max);
-		min = MIN (counts[i], min);
-	}
-
-	if (!(max - min))
-		return FALSE;
-
-	diff = max - min;
-
-	uninit = alloca (buf_len * sizeof (long)); /* Purposely not initialized */
-#warning Are we sufficiently random here ?
-	p_memzero (uninit, buf_len * sizeof (long));
-
-	for (i = 0; i < buf_len; i++) {
-		long diffval;
-		diffval = counts [i] - min;
-
-		buffer [i] ^= (guchar)( ((double) (diffval*256)  / diff )) ^ hashlong(uninit[i]);
-	}
-
-	setitimer (ITIMER_REAL, &oldit, NULL);
-	sigaction (SIGALRM, &oldsa, NULL);
-
-	return TRUE;
+	s ^= t;
 }
 
 void
-ORBit_genrand_buf (ORBit_genrand *gr, guchar *buffer, int buf_len)
+ORBit_genuid_buffer (guchar *buffer, int length)
 {
-	g_return_if_fail (buf_len);
+	g_return_if_fail (length > 0);
 
-	if (gr && gr->fd >=0 && genrand_dev (gr, buffer, buf_len))
-		return;
+	switch (genuid_type) {
+	case ORBIT_GENUID_STRONG:
+		if (genuid_rand_device (buffer, length))
+			return;
 #if LINC_SSL_SUPPORT
-	else if (genrand_openssl (buffer, buf_len))
-		return;
+		else if (genuid_rand_openssl (buffer, length))
+			return;
 #endif
-	else if (genrand_unix (buffer, buf_len))
-		return;
-	else
-		g_error("Couldn't generate random data!");
+		genuid_glib_pseudo (buffer, length);
+		break;
+	case ORBIT_GENUID_SIMPLE:
+		genuid_simple (buffer, length);
+		break;
+	default:
+		break;
+	}
 }
