3e82f506b5dd78d6781bc2a6ac05972ce06a6c1f
Michael Meeks <michael@src.gnome.org>
2002-02-05 15:05:45 +0000

    2002-02-05  Michael Meeks  <michael@ximian.com>
    	* src/orb/orb-core/orbit-object.c
    	(object_hash_dump): impl.
    	(ORBit_RootObject_shutdown),
    	(ORBit_RootObject_init, do_unref): add more verbose
    	and powerful object debugging.
    
    	* src/orb/orb-core/corba-orb.c
    	(ORBit_setup_debug_flags): add 'refs'
    
    	* src/orb/orb-core/orbit-debug.h: fix bitmask cockup.

--- src/orb/orb-core/orbit-object.c
+++ src/orb/orb-core/orbit-object.c
@@ -2,10 +2,61 @@
 #include <stdio.h>
 #include <orbit/orbit.h>
 
+#include "orbit-debug.h"
 #include "orb-core-private.h"
 
 static glong alive_root_objects = 0;
 static glong total_refs = 0;
+
+
+#ifdef G_ENABLE_DEBUG
+static GHashTable *object_hash = NULL;
+
+#define TYPE_CASE(e,n) \
+	case ORBIT_ROT_##e: str = g_strdup (n); break;
+
+static void
+object_hash_dump (gpointer key,
+		  gpointer value,
+		  gpointer user_data)
+{
+	char *str = NULL;
+	ORBit_RootObject obj = key;
+	const ORBit_RootObject_Interface *interface = obj->interface;
+	
+	switch (interface->type) {
+		TYPE_CASE (NULL, "Null");
+	case ORBIT_ROT_OBJREF: {
+		CORBA_Object o = (CORBA_Object) obj;
+		str = g_strdup_printf ("Object (type '%s')",
+				       g_quark_to_string (o->type_qid));
+		break;
+	}
+		/* psuedo-objects */
+		TYPE_CASE (ORB, "ORB");
+		TYPE_CASE (ADAPTOR, "Adaptor");
+		TYPE_CASE (POLICY, "Policy");
+		TYPE_CASE (TYPECODE, "TypeCode");
+		TYPE_CASE (REQUEST, "Request");
+		TYPE_CASE (SERVERREQUEST, "Server Request");
+		TYPE_CASE (CONTEXT, "Context");
+		TYPE_CASE (DYNANY, "Dynany");
+		TYPE_CASE (OAOBJECT, "Object Adaptor Object");
+		TYPE_CASE (ORBGROUP, "ORB Group");
+		TYPE_CASE (POAMANAGER, "POA Manager");
+		TYPE_CASE (POACURRENT, "Current POA");
+	}
+
+	if (!str)
+		str = g_strdup_printf ("Error unknown type '%d'",
+				       interface->type);
+
+	fprintf (stderr, "%3d ref%c to '%s'\n",
+		 obj->refs, obj->refs ? 's' : ' ', str);
+
+	g_free (str);
+}
+#endif
 
 int
 ORBit_RootObject_shutdown (void)
@@ -25,6 +76,13 @@ ORBit_RootObject_shutdown (void)
 	else
 		return 0;
 
+#ifdef G_ENABLE_DEBUG
+	if (_orbit_debug_flags & ORBIT_DEBUG_REFS)
+		g_hash_table_foreach (
+			object_hash, object_hash_dump, NULL);
+#endif
+
+
 	return 1;
 }
 
@@ -34,6 +92,14 @@ ORBit_RootObject_init (ORBit_RootObject obj,
 {
 	if (!ORBit_RootObject_lifecycle_lock) /* No locking */
 		alive_root_objects++;
+
+#ifdef G_ENABLE_DEBUG
+	if (!object_hash)
+		object_hash = g_hash_table_new (NULL, NULL);
+
+	if (_orbit_debug_flags & ORBIT_DEBUG_REFS)
+		g_hash_table_insert (object_hash, obj, obj);
+#endif
 
 	obj->interface = interface;
 	obj->refs = 0;
@@ -78,6 +144,10 @@ do_unref (ORBit_RootObject robj)
 	if (robj->refs == 0) {
 		if (!ORBit_RootObject_lifecycle_lock) /* No locking */
 			alive_root_objects--;
+#ifdef G_ENABLE_DEBUG
+		if (_orbit_debug_flags & ORBIT_DEBUG_REFS)
+			g_hash_table_remove (object_hash, robj);
+#endif
 
 		if (robj->interface && robj->interface->destroy)
 			robj->interface->destroy (robj);
