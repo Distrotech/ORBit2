8454c61fe7f6f902ee230e7ea11d7068b3959930
ÉRDI Gergo <cactus@src.gnome.org>
2002-03-14 23:23:44 +0000

    Added missing header for struct_simple implementation

--- test/cpp/struct-simple/client.cc
+++ test/cpp/struct-simple/client.cc
@@ -26,16 +26,20 @@ main(int argc, char *argv[])
   	  Test::TestIface_var test_iface_ptr = Test::TestIface::_narrow (test_iface_obj);
   	  Test::OutputStream_var stream_ptr = Test::OutputStream::_narrow (stream_obj);
 
+	  std::cout << "Calling test_ret on remote object" << std::endl;
+	  Test::SimpleStructArg *ret_struct = test_iface_ptr->test_ret ();
+	  
   	  const char* message = "TestIface server, from client!";
 	  Test::SimpleStructArg test_struct;
 	  test_struct.stream = stream_ptr;
-	  test_struct.number = 10;
-
+	  test_struct.number = ret_struct->number;
+	  
+	  std::cout << "Calling test_in on remote object" << std::endl;
 	  test_iface_ptr->test_in (test_struct, message);
 
   	  //std::cout << "Client: Reply was \"" << reply << "\"" << std::endl;
 
-	} catch(const CORBA::Exception& ex) {
+	} catch (const CORBA::Exception& ex) {
 		
        std::cerr << "Exception caught. "
 				 << "Maybe the server is not running, "
