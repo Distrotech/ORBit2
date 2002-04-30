f384b19fbc7fd80106078d7c3fc25253ba128cf3
ERDI Gergo <cactus@cactus.rulez.org>
2002-04-30 20:17:47 +0000

    New implementation
    2002-04-30  ERDI Gergo  <cactus@cactus.rulez.org>
    
    	* src/orb-cpp/orbitcpp_sequence.hh: New implementation

--- src/orb-cpp/orbitcpp_sequence.hh
+++ src/orb-cpp/orbitcpp_sequence.hh
@@ -1,5 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
-
+// -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
 /*
  *  ORBit-C++: C++ bindings for ORBit.
  *
@@ -19,254 +18,238 @@
  *  License along with this library; if not, write to the Free
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  Author: Phil Dawes <philipd@users.sourceforge.net>
- *			Andreas Kloeckner <ak@ixion.net>
+ *  Authors: Phil Dawes <philipd@users.sourceforge.net>
+ *           Andreas Kloeckner <ak@ixion.net>
+ *           Gergõ Érdi <cactus@cactus.rulez.org>
  */
-
-
-
 
 #ifndef __ORBITCPP_SEQUENCE_HH
 #define __ORBITCPP_SEQUENCE_HH
 
-#include <cstddef>
-#include <cassert>
-#include "orbitcpp_smartptr.hh"
-#include <orbit/orb-core/orb-core.h>
-
-
-
+#include "orbitcpp_types.hh"
+#include <glib.h> // for g_assert
 
 namespace _orbitcpp {
-	template<class T, class CSEQTYPE>
-	class UnboundedSequenceTmpl;
-	template<class T, class CSEQTYPE, CORBA::ULong max>
-	class BoundedSequenceTmpl;
 
-	template<class SEQ,class T, class CSEQTYPE,CORBA::ULong CapBounds = 0>
-	class SequenceBase {
-	public:
-		typedef T _orbitcpp_value_type;
+    template<typename CPPElem, typename CElem, typename CType, typename Traits>
+    class SequenceBase
+    {
+    public:
+	typedef CPPElem                                      value_t;
+	typedef CElem                                        c_value_t;
+	typedef SequenceBase<CPPElem, CElem, CType, Traits>  self_t;
+	typedef value_t                                     *buffer_t;
+	typedef CType                                        c_seq_t;
+	typedef Traits                                       traits_t;
 	
-	protected:
-		struct {
-			CORBA::ULong	_maximum,_length;
-			T				*_buffer;
-			CORBA::Boolean	_release;
-		} m_target;
+	typedef CORBA::ULong size_t;
+	typedef CORBA::ULong index_t;
 	
-	public:
-		static void freebuf(T *data) {
-			CORBA_free(data);
-		}
-
-		// construction and destruction
-		SequenceBase() { 
-			m_target._length = 0;
-		}
-		SequenceBase(SequenceBase const &src) {
-			m_target = src.m_target;
-			
-			m_target._buffer = SEQ::allocbuf(m_target._maximum);
-			if (!m_target._buffer) throw CORBA::NO_MEMORY();
-			
-			try {
-				for (CORBA::ULong i = 0;i<m_target._length;i++) 
-					m_target._buffer[i] = src.m_target._buffer[i];
-			}
-			catch (...) {
-				SEQ::freebuf(m_target._buffer);
-				throw;
-			}
-			
-			_orbitcpp_set_internal_release_flag(true);
-			
-		}
-		SequenceBase(CORBA::ULong max,CORBA::ULong length,T *data,CORBA::Boolean rel) {
-			assert (length <= max);
-		
-			m_target._maximum = max;
-			m_target._length = length;
-			m_target._buffer = data;
-		
-			_orbitcpp_set_internal_release_flag(rel);
-		}
-		~SequenceBase() {
-			if (release()) SEQ::freebuf(m_target._buffer);
-			// this must be done for the following delete to work ok.
-			m_target._buffer = NULL;
-			m_target._length = 0;
-			m_target._maximum = 0;
-		}
-		
-		// op=
-		SequenceBase &operator=(SequenceBase const &src) {
-			T *buffer = NULL;
-			if(src.m_target._maximum != 0) {
-				buffer = SEQ::allocbuf(src.m_target._maximum);
-				if (!buffer) throw CORBA::NO_MEMORY();
-			}
-			try {
-				for (CORBA::ULong i = 0;i < src.m_target._length;i++)
-					buffer[i] = src.m_target._buffer[i];
-			}
-			catch (...) {
-				SEQ::freebuf(buffer);
-				throw;
-			}
-			
-			m_target._length = src.m_target._length;
-			m_target._maximum = src.m_target._maximum;
-			
-			if (release()) SEQ::freebuf(m_target._buffer);
-			m_target._buffer = buffer;
-			_orbitcpp_set_internal_release_flag(true);
-			return *this;
-		}
-		
-		// property/contents access
-		CORBA::Boolean release() const {
-			return CORBA_sequence_get_release(_orbitcpp_get_c_sequence_ptr());
-		}
-		CORBA::ULong maximum() const {
-			return m_target._maximum;
-		}
-		CORBA::ULong length() const {
-			return m_target._length;
-		}
-		T &operator[](CORBA::ULong idx) {
-			return m_target._buffer[idx];
-		}
-		const T &operator[](CORBA::ULong idx) const {
-			return m_target._buffer[idx];
-		}
-
-		// begin ORBIT-C++ extension
-		void _orbitcpp_set_internal_release_flag (CORBA::Boolean rel) {
-			CORBA_sequence_set_release(&m_target,rel);
-		}
-		CSEQTYPE* _orbitcpp_get_c_sequence_ptr() const {
-			// this cast looks nasty, and rightly so!
-			return const_cast<CSEQTYPE*>(reinterpret_cast<const CSEQTYPE*>(&m_target));
-		}
-	};
-
-	// --------------------- Unbounded sequence type ---------------------------
+    private:
+	size_t   _max;
+	size_t   _length;
+	buffer_t _buffer;
+	bool     _release;
 	
-	template <class T, class CSEQTYPE>
-	class UnboundedSequenceTmpl :
-		public SequenceBase<UnboundedSequenceTmpl<T,CSEQTYPE>,T,CSEQTYPE> {
-		typedef SequenceBase <UnboundedSequenceTmpl<T,CSEQTYPE>,T,CSEQTYPE> Super;
-	public:
-
-		UnboundedSequenceTmpl() {
-			m_target._maximum = 0;
-			m_target._length = 0;
-			m_target._buffer = NULL;
-			CORBA_sequence_set_release(&m_target,TRUE);
-		}
-		UnboundedSequenceTmpl(CORBA::ULong max) {
-			m_target._maximum = max;
-			m_target._length = 0;
-			m_target._buffer = allocbuf(max);
-			if (!m_target._buffer) throw CORBA::NO_MEMORY();
-			
-			CORBA_sequence_set_release(&m_target,TRUE);
-		}
-		UnboundedSequenceTmpl(CORBA::ULong max, CORBA::ULong length, T *value,
-					 CORBA::Boolean rel = FALSE)
-			: Super(max,length,value,rel) {
-		}
+    public:
+	
+	// Empty constructor
+	SequenceBase (size_t max = 0):
+	    _max (max),
+	    _length (0),
+	    _buffer (max ? new value_t[max] : 0),
+	    _release (true)
+	    {
+	    }
+	
+	// Create sequence from flat buffer
+	SequenceBase (size_t max,
+				  size_t length, buffer_t buffer,
+				  CORBA::Boolean release = false):
+	    _max (max),
+	    _length (length),
+	    _buffer (buffer),
+	    _release (release)
+	    {
+			g_assert (length <= max);
+	    }
+	
+	// Copy constructor
+	SequenceBase (const self_t &src):
+	    _max (src._max),
+	    _length (src._length),
+	    _release (true)
+	    {
+		_buffer = allocbuf (_max);
+		if (!_buffer)
+		    throw CORBA::NO_MEMORY ();
 		
-		// need this because of dumb overriding rule
-		CORBA::ULong length() const {
-			return Super::length();
-		}
-		void length(CORBA::ULong len) {
-			if (len > m_target._maximum) {
-				T *buffer = allocbuf(len);
-				if (!buffer) throw CORBA::NO_MEMORY();
+		for (index_t i = 0; i < _length; i++)
+		    _buffer[i] = src._buffer[i];
+	    }
+	
+	// Copy operator
+	self_t & operator= (const self_t &src) {
+	    buffer_t buffer_tmp = 0;
+	    
+	    if (src._max != 0) {
+		buffer_tmp = allocbuf (src._max);
+		if (!buffer_tmp)
+		    throw CORBA::NO_MEMORY ();
+	    }
+	    
+	    for (index_t i = 0; i < src._length; i++)
+		buffer_tmp[i] = src._buffer[i];
+	    
+	    _length = src._length;
+	    _max = src._max;
+	    
+	    if (_release)
+		freebuf (_buffer);
+	    _buffer = buffer_tmp;
+	    
+	    return *this;
+	}
+	
+	// Destructor
+	~SequenceBase () {
+	    if (_release)
+		freebuf (buffer);
+	}
+	
+	// Size information
+	size_t maximum () const { return _max;    };
+	size_t length () const  { return _length; };
+	
+	// Element access
+	value_t& operator[] (index_t index)             { return _buffer[index]; };
+	const value_t& operator[] (index_t index) const { return _buffer[index]; };
+	
+	// Memory managment
+	static buffer_t allocbuf (size_t num_elems) { return new value_t[num_elems]; };
+	static void freebuf (buffer_t buffer)       { delete buffer;                 };    
+	
+	// ORBit2/C++ extension: create C sequence
+	c_seq_t* _orbitcpp_pack () const {
+	    c_seq_t *retval = traits_t().alloc_c ();
+	    _orbitcpp_pack (*retval);
+	    return retval;
+	}
+
+	// ORBit2/C++ extension: fill C sequence
+	void _orbitcpp_pack (c_seq_t &_c_seq) const {
+	    for (index_t i = 0; i < _length; i++)
+		traits_t().pack_elem (_buffer[i], _c_seq._buffer[i]);
+	}
+	
+	// ORBit2/C++ extension: fill C++ sequence from C sequence
+	void _orbitcpp_unpack (const c_seq_t &_c_seq) {
+	    for (index_t i = 0; i < _length; i++)
+		traits_t().unpack_elem (_buffer[i], _c_seq._buffer[i]);
+	}
+    };
+    
+    template<typename CPPElem, typename CElem, typename CType, typename Traits>
+    class UnboundedSequence: public SequenceBase<CPPElem, CElem, CType, Traits>
+    {
+		typedef SequenceBase<CPPElem, CElem, CType, Traits> Super;
+		
+    public:
+		// Empty constructor
+		UnboundedSequence (size_t max = 0):
+			Super (max)
+			{
+			}
+		
+		// Create sequence from flat buffer
+		UnboundedSequence (size_t max,
+						   size_t length, buffer_t buffer,
+						   CORBA::Boolean release = false):
+			Super (max, length, buffer, release)
+			{
+			}
+		
+		// Copying
+		UnboundedSequence (const self_t &other):
+			Super (other)
+			{
+			}
+		
+		// Size requisition
+		void length (size_t new_length) {
+			if (new_length > _max)
+			{
+				buffer_t buffer_tmp = allocbuf (new_length);
+				if (!buffer_tmp)
+					throw CORBA::NO_MEMORY ();
 				
-				for (CORBA::ULong i = 0;i<m_target._length;i++) 
-					buffer[i] = m_target._buffer[i];
-
-				if (release()) 
-					freebuf(m_target._buffer);
-				m_target._buffer = buffer;
-				m_target._maximum = len;
-				_orbitcpp_set_internal_release_flag(true);
+				for (index_t i = 0; i < _length; i++)
+					buffer_tmp[i] = _buffer[i];
+				
+				if (_release)
+					freebuf (_buffer);
+				
+				_buffer = buffer_tmp;
+				_max = new_length;
 			}
-			m_target._length = len;
-		}
-		
-		// new and allocBuf defined through partial specialization
-		void* operator new(size_t size);
-		void operator delete(void* c_sequence) {
-			CORBA_free(c_sequence);
-		}
-		static T *allocbuf(CORBA::ULong size);
-
-		// copy ctor and op= as generated
-	};
-
-	// --------------------- Bounded sequence type ---------------------------
-	
-	template<class T, class CSEQTYPE,CORBA::ULong max>
-	class BoundedSequenceTmpl :
-		public SequenceBase<BoundedSequenceTmpl<T,CSEQTYPE,max>,T,CSEQTYPE,max> {
-		typedef SequenceBase<BoundedSequenceTmpl<T,CSEQTYPE,max>,T,CSEQTYPE,max> Super;
-	public:	
-		BoundedSequenceTmpl() {
-			m_target._maximum = max;
-			m_target._buffer = allocbuf(max);
-			if (!m_target._buffer) throw CORBA::NO_MEMORY();
 			
-			CORBA_sequence_set_release(&m_target,TRUE);
+			_length = new_length;
 		}
-		BoundedSequenceTmpl(CORBA::ULong length, T *value,CORBA::Boolean rel = FALSE)
-			: Super(max,length,value,rel) {
+    };
+    
+    template<typename CPPElem, typename CElem, typename CType, typename Traits,
+	     SequenceBase<CPPElem, CElem, CType, Traits>::size_t max>
+    class BoundedSequence: public SequenceBase<CPPElem, CElem, CType, Traits>
+    {
+		typedef SequenceBase<CPPElem, CElem, CType, Traits> Super;
+		
+    public:
+		// Empty constructor
+		BoundedSequence ():
+			Super (max)
+			{
+			}
+		
+		// Create sequence from flat buffer
+		BoundedSequence (size_t length, buffer_t buffer,
+						 CORBA::Boolean release = false):
+			Super (max, length, buffer, release)
+			{
+			}
+		
+		// Copying
+		BoundedSequence (const self_t &other):
+			Super (other)
+			{
+			}
+		
+		// Size requisition
+		void length (size_t new_length) {
+			g_assert (new_length <= _max);
+			
+			_length = new_length;
 		}
+    };
+} // namespace _orbitcpp
 
-		// need this because of dumb overriding rule
-		CORBA::ULong length() const {
-			return Super::length();
-		}
-		void length(CORBA::ULong l) {
-			m_target._length = l;
-		}
+#define ORBITCPP_DECLARE_SIMPLE_SEQUENCE(CPPType, CPPElem, CElem, CType)	\
+    struct CPPType##_traits							\
+    {										\
+	CType* alloc_c () const {						\
+	    return CType##__alloc ();						\
+	}									\
+										\
+	void pack_elem (CPPElem &cpp_value, CElem &c_value) const {		\
+	    c_value = (CElem) cpp_value;					\
+	}									\
+										\
+	void unpack_elem (CPPElem &cpp_value, CElem &c_value) const {		\
+	    cpp_value = (CPPElem) c_value;					\
+	}									\
+    };										\
+										\
+    typedef ::_orbitcpp::UnboundedSequence<CPPElem, CElem, CType, CPPType##_traits> CPPType; \
+    typedef ::_orbitcpp::Sequence_var< CPPType > CPPType##_var;			\
+    typedef ::_orbitcpp::Sequence_out< CPPType > CPPType##_out;
 
-		// new and allocBuf defined through partial specialization
-		void* operator new(size_t size);
-		void operator delete(void* c_sequence) {
-			CORBA_free(c_sequence);
-		}
-		static T *allocbuf(CORBA::ULong size);
-
-		// copy ctor and op= as generated
-	};	
-}
-
-
-
-
-#define ORBITCPP_DECLARE_SIMPLE_SEQUENCE(COMPONENT,ALLOC_FUNC,ALLOCBUF_FUNC,C_NAME,NAME) \
-	typedef _orbitcpp::UnboundedSequenceTmpl< COMPONENT,C_NAME> NAME; \
-	inline void *NAME::operator new(size_t size) { \
-		/* Keep the compiler quiet. */ \
-		(void) size; \
-		return ALLOC_FUNC(); \
-	} \
-	inline void NAME::operator delete(void *seq) { \
-		CORBA_free(seq); \
-	} \
-	inline COMPONENT *NAME::allocbuf(CORBA::ULong len) { \
-		return reinterpret_cast< COMPONENT *>( \
-		  ALLOCBUF_FUNC(len)); \
-	} \
-	typedef ::_orbitcpp::Sequence_var< NAME> NAME##_var; \
-	typedef ::_orbitcpp::Sequence_out< NAME> NAME##_out; \
-
-
-
-
-#endif
+#endif // !__ORBITCPP_SEQUENCE_HH
