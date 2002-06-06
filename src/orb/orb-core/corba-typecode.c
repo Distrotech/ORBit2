419f49e33f50a767720896949b71e41efb0c6eae
Michael Meeks <michael@src.gnome.org>
2002-06-06 11:34:08 +0000

    2002-06-06  Michael Meeks  <michael@ximian.com>
    	* src/orb/orb-core/corba-typecode.c: update
    	TC_null's name #84354

--- src/orb/orb-core/corba-typecode.c
+++ src/orb/orb-core/corba-typecode.c
@@ -81,7 +81,7 @@ typedef struct {
 const struct CORBA_TypeCode_struct TC_null_struct = {
 	{&ORBit_TypeCode_epv, ORBIT_REFCOUNT_STATIC},
 	CORBA_tk_null, 0, 0, -1, 0, 0, NULL,
-	CORBA_OBJECT_NIL, "null", "Null"
+	CORBA_OBJECT_NIL, "null", "IDL:omg.org/CORBA/Null:1.0"
 };
 
 const struct CORBA_TypeCode_struct TC_void_struct = {
