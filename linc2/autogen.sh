51693abc3123230b8f9bf53be1c7d52bea092b13
Gustavo J. A. M. Carneiro <gcarneiro@src.gnome.org>
2003-04-28 14:41:10 +0000

    docs

--- linc2/autogen.sh
+++ linc2/autogen.sh
@@ -4,78 +4,43 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
+PKG_NAME="libbonobo"
+REQUIRED_AUTOMAKE_VERSION=1.6
+REQUIRED_AUTOCONF_VERSION=2.12
+
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/autogen.sh \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
 
 DIE=0
 
-# Check for autoconf, the required version is set in configure.in
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have at minimum autoconf version 2.12 installed"
-	echo "to compile ORBit. Download the appropriate package for"
-	echo "your distribution, or get the source tarball at"
-	echo "ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
+# This is a bit complicated here since we can't use gnome-config yet.
+# It'll be easier after switching to pkg-config since we can then
+# use pkg-config to find the gnome-autogen.sh script.
 
-# Check for libtool
-(libtool --version | egrep "1.3") > /dev/null || 
-(libtool --version | egrep "1.4") > /dev/null || {
-	echo
-	echo "You must have at minimum libtool version 1.3 installed"
-	echo "to compile ORBit. Download the appropriate package for"
-	echo "your distribution, or get the source tarball at"
-	echo "ftp://alpha.gnu.org/gnu/libtool-1.4.tar.gz"
-	DIE=1
-}
+gnome_autogen=
+gnome_datadir=
 
-# Check for automake, the required version is set in Makefile.am
-(automake-1.4 --version) < /dev/null > /dev/null 2>&1 ||{
-	echo
-	echo "You must have at minimum automake version 1.4 installed"
-	echo "to compile ORBit. Download the appropriate package for"
-	echo "your distribution, or get the source tarball at"
-	echo "ftp://ftp.cygnus.com/pub/home/tromey/automake-1.4.tar.gz"
-	DIE=1
-}
-
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-(test -f src/linc.c) || {
-	echo "You must run this script in the top-level ORBit directory"
-	exit 1
-}
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
-
-for i in .
-do 
-  echo processing $i
-  (cd $i; \
-    libtoolize --copy --force; \
-    aclocal-1.4 $ACLOCAL_FLAGS;
-    autoheader; \
-    automake-1.4 --add-missing $am_opt; \
-    autoheader; \
-    autoconf)
+ifs_save="$IFS"; IFS=":"
+for dir in $PATH ; do
+  test -z "$dir" && dir=.
+  if test -f $dir/gnome-autogen.sh ; then
+    gnome_autogen="$dir/gnome-autogen.sh"
+    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
+    break
+  fi
 done
+IFS="$ifs_save"
 
-cd $ORIGDIR
+if test -z "$gnome_autogen" ; then
+  echo "You need to install the gnome-common module and make"
+  echo "sure the gnome-autogen.sh script is in your \$PATH."
+  exit 1
+fi
 
-echo "Running $srcdir/configure --enable-maintainer-mode" "$@"
-$srcdir/configure --enable-maintainer-mode "$@"
+GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
 
-echo 
-echo "Now type 'make' to compile linc."
